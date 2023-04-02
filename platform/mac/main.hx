c3442dc11313ebca2048df9b9cba89b337542440
AlvarroPewz <115935861+AlvarroPewz24@users.noreply.github.com>
2023-04-02 20:21:22 +0700

    Update platform/mac/main.hx

--- platform/mac/main.hx
+++ platform/mac/main.hx
@@ -4,7 +4,7 @@ import haxe.fpsloader;
 import haxe.gamefps;
 
 run: "apis/FPSLoader.hx"
-run-command: $check_running-apps
+run-command: $check-running-apps
              $check-appdirectory
              $delete fps.xml
              $create $fps-name.xml
