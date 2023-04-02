09225f8e8aba9c7198918b9c40459c13872a6a0e
JackR <120707466+jackrayYml@users.noreply.github.com>
2023-04-02 19:48:30 +0700

    forgot it was a hx format

--- platform/windows/main.hx
+++ platform/windows/main.hx
@@ -1,7 +1,16 @@
-- win10+
-run: "apis/FPSLoader.hx"
-delete: (C:Users/$windows-user/$application-run="app-name"$checkdirectory/fps.xml)
-replace with: ($fps-name).xml
+package;
 
-- win8-
-run-message = "We do not support any Windows 8- We only supported win10+"
+import haxe.fps.loader
+import game.fps
+
+  run: "apis/FPSLoader.hx"
+
+  #if win10+
+  run: "apis/FPSLoader.hx"
+  delete: (C:Users/$windows-user/$application-run="app-name"$checkdirectory/fps.xml)
+  replace with: ($fps-name).xml
+  #end
+
+  #if win8-
+  show-message: "We do not support any Windows 8- We only supported win10+"
+  #end
