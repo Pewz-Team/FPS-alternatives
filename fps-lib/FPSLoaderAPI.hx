0cec4a7cbde518c8582f627db15f617abc3a90c8
AlvarroPewz <115935861+AlvarroPewz24@users.noreply.github.com>
2023-04-02 16:10:14 +0700

    Create FPSLoaderAPI.hx

---
+++ fps-lib/FPSLoaderAPI.hx
@@ -0,0 +1,8 @@
+package;
+
+class FPSLoaderAPI
+{
+	inline public static var API:String = "85075:WqA67t";
+	inline public static var EncKey:String = "6A9UD0DYW1";
+	inline public static var SESSION:String = null;
+}
