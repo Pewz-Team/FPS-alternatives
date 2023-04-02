98d9a9a012c65321e7f870525be7e3681fa7711c
JackR <120707466+jackrayYml@users.noreply.github.com>
2023-04-02 19:52:50 +0700

    same as before

--- platform/linux/main.hx
+++ platform/linux/main.hx
@@ -1,7 +1,16 @@
-- 64-bit
-auto-run: "apis/FPSLoader.hx
+package;
+
+import haxe.fpsloader
+import haxe.game.fps
+
+run: "apis/FPSLoader.hx"
+
+#if 64-bit
+run: "apis/FPSLoader.hx
 run-command: $sudo update $game_id delete fps.xml
              $sudo update $game_id create $fps-name.xml
+#end
 
-- 32-bit
+#if 32-bit
 show-message: "Sorry we do not support linux 32bits, maybe supported in the future?
+#end
