6bc51499eb0870f5b1f88bcb0bd8761b0da29e03
JackR <120707466+jackrayYml@users.noreply.github.com>
2023-04-02 19:42:00 +0700

    Update main.hx

--- platform/windows/main.hx
+++ platform/windows/main.hx
@@ -2,7 +2,6 @@
 run: "apis/FPSLoader.hx"
 delete: (C:Users/$windows-user/$application-run="app-name"$checkdirectory/fps.xml)
 replace with: ($fps-name).xml
-#end
 
 - win8-
 run-message = "We do not support any Windows 8- We only supported win10+"
