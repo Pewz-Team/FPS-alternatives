70fc97915c5cbf1bfea77c94573ca0673887f180
JackR <120707466+jackrayYml@users.noreply.github.com>
2023-04-02 19:31:53 +0700

    linux is out!1!1!!1

---
+++ platform/linux/main.hx
@@ -0,0 +1,6 @@
+- 64-bit
+run: $sudo update $game_id delete fps.xml
+     $sudo update $game_id create $fps-name.xml
+
+- 32-bit
+show-message: "Sorry we do not support linux 32bits, maybe supported in the future?
